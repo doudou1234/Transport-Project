68c39f661446392113e1ff3f4359eec817eb0769
doudou1234 <1790137419@qq.com>
2017-12-17 15:59:49 +0800

    update rtrt

--- src/youxi/rtrt.java
+++ src/youxi/rtrt.java
@@ -1,11 +1,34 @@
 package youxi;
-
 public class rtrt
 {
 
     
-	weiurehgfh
-    fdff;
+
+	private int age;
+	private String name;
+	public rtrt()
+	{
+		this(10);
+	}
+	public rtrt(int age)
+	{
+		this.age=age;	
+	}
+	public void setName(String name) 	
+	{
+		this.name=name;
+	}
+
+	public void print() 	
+	{
+		System.out.println(this.age);
+	}
+	
+	public static void main(String[] args)
+	{
+		rtrt demo=new rtrt();
+		demo.print();
+	}
   
   
 }
